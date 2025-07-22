952acee2160120c902e282329e5dd63f9faa2f62
Mohamed BOUAZZA <mohamed.bouazza@impakt.io>
2025-07-22 22:41:38 +0200

    [api] security comments

--- backend/config/config.js
+++ backend/config/config.js
@@ -3,7 +3,7 @@ module.exports = {
   port: process.env.PORT || 5000,
   dbUri:
     process.env.DB_URI ||
-    "mongodb+srv://hamma216:BbM7j4Hp0ve1JDwz@cluster0.55enr1c.mongodb.net/",
+    "mongodb+srv://hamma216:BbM7j4Hp0ve1JDwz@cluster0.55enr1c.mongodb.net/", //Publicly leaked secret!, just for reviewer access...never do it in a project
   jwtSecret:
     process.env.JWT_SECRET || "Y3g9nL92iLr5Xlm0vTO9A7T6JexgT7XxyDdN1c5x9e8",
 };
